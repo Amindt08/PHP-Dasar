0480f0a63d48263c5d6037bf15976659a233539a
Amindita Maestri <151582472+Amindt08@users.noreply.github.com>
2024-05-01 20:05:47 +0700

    Modul 8 - PHP Dasar

--- latihan6.php
+++ latihan6.php
@@ -1,33 +1,26 @@
 <?php
-//while loop
+
 $x = 10;
-while ($x >= 5) {
-    echo "Nomor : $x <br>";
+while ($x < 5){
+    echo "Nomor = $x <br>";
     $x--;
 }
 
-// do while
 $x = 1;
-do {
+do{
     echo "Nomor : $x <br>";
     $x++;
-} while ($x <= 5);
+}while($x <= 5);
 
-//foreach
 $colors = array("red", "green", "pink", "yellow");
-foreach ($colors as $value) {
+foreach($colors as $value) {
     echo "$value <br>";
 }
 
-//for 
-for ($x = 0; $x <= 10; $x++) {
-    echo "Nomor : $x <br>";
-}
-
-//for dengan break
-for ($x = 0; $x < 10; $x++) {
-    if ($x == 4) {
+for ($x = 0; $x < 10; $x++){
+    if($x==4){
         break;
     }
     echo "Nomor : : $x <br>";
 }
+?>
